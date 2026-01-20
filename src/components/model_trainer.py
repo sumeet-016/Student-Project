421ac5b7800330f0ea357076ba4773076fbfba87
Sumeet Kumar Pal <pal.sumeetkumar@gmail.com>
2026-01-20 16:54:39 +0530

    Hyper parameter tunning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -43,9 +43,48 @@ class ModelTrainer:
                         "Catboost": CatBoostRegressor(verbose = False),
                         "XGBoost": XGBRegressor(),
                         "Random Forest": RandomForestRegressor()}
+
+            params={
+                "Linear Regression":{},
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Random Forest":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boost":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                
+                "Ada Boost":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Catboost":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "XGBoost":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+                
+            }
             
             model_report:dict = evaluate_models(X_train, y_train, X_test,
-                                                y_test, models)
+                                                y_test, models, params)
             
             best_model_score = max(sorted(model_report.values()))
 
@@ -71,4 +110,4 @@ class ModelTrainer:
 
 
         except Exception as e:
-            raise CustomException(e, sys)
+            raise CustomException(e, sys) 
