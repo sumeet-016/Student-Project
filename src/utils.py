05c285fd68285a126601b3d455e55d39b62c41b7
Sumeet Kumar Pal <pal.sumeetkumar@gmail.com>
2026-01-31 17:41:54 +0530

    application completion

--- src/utils.py
+++ src/utils.py
@@ -1,38 +1,42 @@
 import os
 import sys
-import dill
 
-import numpy as np
+import numpy as np 
 import pandas as pd
+import dill
+import pickle
 from sklearn.metrics import r2_score
-from sklearn.model_selection import RandomizedSearchCV
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
+
         os.makedirs(dir_path, exist_ok=True)
 
         with open(file_path, "wb") as file_obj:
-            dill.dump(obj, file_obj)
+            pickle.dump(obj, file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_models(X_train, y_train, X_test, y_test, models, params):
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
         report = {}
 
-        for i in range(len(models)):
+        for i in range(len(list(models))):
             model = list(models.values())[i]
-            para=params[list(models.keys())[i]]
+            para=param[list(models.keys())[i]]
 
-            gs = RandomizedSearchCV(model,para,cv=3)
+            gs = GridSearchCV(model,para,cv=3)
             gs.fit(X_train,y_train)
 
             model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
+
+            y_train_pred = model.predict(X_train)
 
             y_test_pred = model.predict(X_test)
 
@@ -41,6 +45,14 @@ def evaluate_models(X_train, y_train, X_test, y_test, models, params):
             report[list(models.keys())[i]] = test_model_score
 
         return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
